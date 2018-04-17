52ae51c079c2f95641f045545f93c1db83ef72ac
Arvid Nydén <awe.tribute@gmail.com>
2018-04-17 10:24:12 +0800

    force deploy

--- ArvidNyden/ArvidNyden.Server/Program.cs
+++ ArvidNyden/ArvidNyden.Server/Program.cs
@@ -13,7 +13,7 @@ namespace ArvidNyden.Server
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
-        }
+        } 
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
